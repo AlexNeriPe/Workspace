5e8c410e59705c340f2b170d8083ccd63d99512a
AlexNeriPe <alex.neri.pena@gmail.com>
2022-08-28 13:14:53 -0500

    Return route

---
+++ VacuumCleanerRoute/ReturnRoute.java
@@ -0,0 +1,21 @@
+package VacuumCleanerRoute;
+
+import java.util.Scanner;
+
+/**
+ * ReturnRoute
+ */
+public class ReturnRoute {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the route: ");
+        String route = scanner.nextLine();
+
+    }
+
+    public static boolean isVacuumReturn(){
+
+        return false;
+    }
+}
